6de1eb295d0505c11345ab5db3bb012c7b6d205b
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-08-19 13:45:25 -0700

    remove unnecessary console logs

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,18 +7,8 @@ export default Ember.Route.extend({
   actions: {
     askQuestion(params) {
       var newQuestion = this.store.createRecord('question', params);
-      console.log(params);
       newQuestion.save();
       this.transitionTo('index');
-    },
-    answerQuestion(params) {
-      var newAnswer = this.store.createRecord('answer', params);
-      var question = params.question;
-      question.get('answers').addObject(newAnswer);
-      newAnswer.save().then(function() {
-        return question.save();
-      });
-      this.transitionTo('question', params.question);
     }
   }
 });
