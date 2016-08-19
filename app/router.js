70eb1b883bb0718bdd7f58959c7ee490b373cf87
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-08-19 14:28:13 -0700

    add user log in check for user name matching with database

--- app/router.js
+++ app/router.js
@@ -11,6 +11,7 @@ Router.map(function() {
   this.route('ask');
   this.route('question', {path: '/question/:question_id'});
   this.route('signup');
+  this.route('login');
 });
 
 export default Router;
---
+++ app/routes/login.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('user');
+  },
+  actions: {
+    login(model) {
+      var loginName = this.controller.get('userlogin');
+      var searchedNames = [];
+      model.forEach(function(user) {
+        console.log(user.get('username'));
+        searchedNames.push(user.get('username'));
+        if (user.get('username') === loginName) {
+          console.log("Login matches");
+        } else {
+          console.log("User not found");
+        }
+      });
+    }
+  }
+});
