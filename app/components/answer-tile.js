b9816ee7a22739d60d904c077ea7381a5c74e7d9
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-08-21 15:13:03 -0700

    add delete question button on index if question belongs to user

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    deleteAnswer(answer) {
+      answer.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    deleteQuestion(question) {
+      var answer_deletions = question.get('answers').map(function(answer) {
+        return answer.destroyRecord();
+      })
+      Ember.RSVP.all(answer_deletions).then(function() {
+        return question.destroyRecord();
+      })
+      this.transitionTo('index');
+    }
+  }
 });
